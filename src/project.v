2f9f4378f3d129db35adf87741f890b81d432760
Kolli Pravalika <pravalikakolli88@gmail.com>
2026-04-17 11:42:04 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,34 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_rca4 (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire clk,
+    input  wire rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire [3:0] A = ui_in[3:0];
+    wire [3:0] B = ui_in[7:4];
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    wire c1, c2, c3, c4;
+
+    assign uo_out[0] = A[0] ^ B[0];
+    assign c1 = A[0] & B[0];
+
+    assign uo_out[1] = A[1] ^ B[1] ^ c1;
+    assign c2 = (A[1] & B[1]) | (B[1] & c1) | (A[1] & c1);
+
+    assign uo_out[2] = A[2] ^ B[2] ^ c2;
+    assign c3 = (A[2] & B[2]) | (B[2] & c2) | (A[2] & c2);
+
+    assign uo_out[3] = A[3] ^ B[3] ^ c3;
+    assign c4 = (A[3] & B[3]) | (B[3] & c3) | (A[3] & c3);
+
+    assign uo_out[4] = c4;
+    assign uo_out[7:5] = 3'b000;
+
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
